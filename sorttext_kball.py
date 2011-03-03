31f1e8a27ed12f763a954e8cbebe5637c9e28734
Kevin Ball <kball@fashioningchange.com>
2011-03-03 15:12:47 -0800

    Kball example sort text & test file

---
+++ sorttext_kball.py
@@ -0,0 +1,20 @@
+import string
+
+def words(text):
+    for c in string.punctuation:
+        text = text.replace(c,"")
+    print text.split()
+    return map(lambda x: x.lower(), text.split())
+
+
+
+file = raw_input("What file would you like to sort? ")
+strs = {}
+text = str(open(file, 'r').read())
+for word in words(text):
+  if (not strs.has_key(word)):
+    strs[word] = 0
+  strs[word] += 1
+for word in sorted(strs.keys()):
+  print word + " : " + str(strs[word])
+
